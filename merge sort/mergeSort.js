feaa0e9d83bc4308e5a2d40e42ded9d5fb8208e7
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-07-01 15:28:37 -0500

    Add function to merge two sorted arrays together

---
+++ merge sort/mergeSort.js
@@ -0,0 +1,28 @@
+function merge(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  const result = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    }
+    if (arr1[j] < arr2[i]) {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+function mergeSort(arr) {}
